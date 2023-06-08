5bee3bd057c2b9cc1cae938055431513d30fca6e
Tran Manh <manh@nimblehq.co>
2023-06-08 11:41:21 +0700

    [#5] Config deploy-preview and deploy workflow to deploy app to production

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -6,6 +6,8 @@ const HomeScreen = (): JSX.Element => {
   return (
     <div className="app">
       <header className="app-header">
+        {/* TODO: This is the test line for .env file. Will remove it later. */}
+        <div>{process.env.REACT_APP_DUMMY_ENV}</div>
         <img src={logo} className="app-logo" alt="logo" />
         <a className="app-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" data-test-id="app-link">
           Learn React
