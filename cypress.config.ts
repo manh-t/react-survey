d2fbf6cfb1280d67d30a54c57a86d8745c98ac28
Tran Manh <manh@nimblehq.co>
2023-06-13 11:16:44 +0700

    [#3] Update Cypress config so the test wont fail

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,22 +1,28 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable import/extensions */
+import browserify from '@cypress/browserify-preprocessor';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  'cypress-react-selector': {
-    root: '#root',
-  },
   retries: {
     runMode: 2,
     openMode: 0,
   },
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.ts')(on, config);
-    },
     baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.spec.ts',
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config);
+      on(
+        'file:preprocessor',
+        browserify({
+          typescript: require.resolve('typescript'),
+        })
+      );
+
+      // It's IMPORTANT to return the config object
+      // with any changed environment variables
+      return config;
+    },
   },
 });
