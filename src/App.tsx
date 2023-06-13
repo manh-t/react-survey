1f5ecd0defd8a23419d58a0c703b16672a6bc59b
Tran Manh <manh@nimblehq.co>
2023-06-13 10:26:26 +0700

    [#1] Update css to scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useRoutes } from 'react-router-dom';
 
-import 'dummy.css';
-import 'assets/stylesheets/application.css';
+import 'dummy.scss';
+import 'assets/stylesheets/application.scss';
 
 import routes from 'routes';
 
