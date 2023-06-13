c32e242496da05fe5d1f500d641e3418e63c5786
Tran Manh <manh@nimblehq.co>
2023-06-13 14:37:57 +0700

    [#6] Add Forgot text to the password field

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -9,18 +9,22 @@ interface TextInputProps {
     placeholder?: string;
     'data-test-id'?: string;
   };
+  extraClassName?: string;
 }
-const TextInput = ({ label, inputAttributes }: TextInputProps): JSX.Element => {
+const TextInput = ({ label, inputAttributes, extraClassName }: TextInputProps): JSX.Element => {
   return (
     <div>
       {label !== null && (
-        <label className="text-white text-[15px] font-extrabold" htmlFor={inputAttributes.id}>
+        <label className="text-white text-small font-extrabold" htmlFor={inputAttributes.id}>
           {label}
         </label>
       )}
       <input
         {...inputAttributes}
-        className="block appearance-none bg-white bg-opacity-[.18] rounded-[12px] w-full h-14 focus:outline-none focus:ring-transparent px-3 text-white text-[17px]"
+        className={
+          'block appearance-none bg-white bg-opacity-[.18] rounded-[12px] w-full h-14 focus:outline-none focus:ring-transparent px-3 text-white text-regular ' +
+          extraClassName
+        }
         placeholder={inputAttributes.placeholder}
       />
     </div>
