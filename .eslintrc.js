f78cebdc6e6364e2649488577c9741372a9fe869
Tran Manh <manh@nimblehq.co>
2023-06-13 17:07:10 +0700

    [#6] Add unittest for SignInScreen

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
-  extends: ['@nimblehq/eslint-config-nimble-react', '@nimblehq/eslint-config-nimble-testing'],
+  extends: [
+    '@nimblehq/eslint-config-nimble-react',
+    '@nimblehq/eslint-config-nimble-testing',
+    'plugin:cypress/recommended',
+    'prettier',
+  ],
   rules: {
     // Note: you must disable the base rule as it can report incorrect errors
     'no-shadow': 'off',
