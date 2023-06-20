696d5d55d3c1e9ec460674eb58f7c7c01884ae20
Tran Manh <manh@nimblehq.co>
2023-06-20 16:54:39 +0700

    [#9] Create the empty dashboard

---
+++ src/components/Dashboard/Header/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { ReactComponent as NimbleLogoWhite } from 'assets/images/icons/nimble-logo-white.svg';
+
+interface DashboardHeaderProps {
+  dateTime: string;
+  daysAgo: string;
+  profileUrl: string;
+  children: React.ReactNode;
+}
+const DashboardHeader = ({ dateTime, daysAgo, profileUrl, children }: DashboardHeaderProps): JSX.Element => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <div className="flex flex-row justify-between">
+        <NimbleLogoWhite />
+        <img className="w-[36px] h-[36px] rounded-full" src={profileUrl} alt="user avatar" />
+      </div>
+      <div className="flex flex-row justify-between">
+        <div className="w-1/5"></div>
+        <div className="flex flex-col text-white mt-10 flex-1">
+          <p className="text-x-small font-extrabold">{dateTime}</p>
+          <p className="text-x-large font-extrabold mt-1">{daysAgo}</p>
+        </div>
+        <div className="w-1/5"></div>
+      </div>
+      <div className="flex flex-row justify-between mt-8 grow">
+        <div className="w-1/5"></div>
+        <div className="flex-1">{children}</div>
+        <div className="w-1/5"></div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardHeader;
