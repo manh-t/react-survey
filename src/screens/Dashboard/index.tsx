cd8eccb17bc1c54fcc15b8a4d3c788001d9215ce
Tran Manh <manh@nimblehq.co>
2023-06-21 14:39:42 +0700

    [#9] Update layout for content

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -7,7 +7,7 @@ const DashBoardScreen = (): JSX.Element => {
   return (
     <div className="bg-black w-full h-full pt-8 pl-8 pr-8">
       <DashboardHeader dateTime="Monday, JUNE 15" daysAgo="Today" profileUrl="https://i.pravatar.cc/150?img=3">
-        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="w-full h-full flex justify-center mt-36">
           <DashboardEmpty />
         </div>
       </DashboardHeader>
