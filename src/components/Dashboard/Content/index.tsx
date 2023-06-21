3ff91ee0924b139dcc207d44ee64efbdfef6dd49
Tran Manh <manh@nimblehq.co>
2023-06-21 16:03:21 +0700

    [#10] Create Shimmer loading

---
+++ src/components/Dashboard/Content/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Shimmer from 'components/Shimmer';
+
+const DashboardContent = (): JSX.Element => {
+  return (
+    <div className="flex flex-col w-full">
+      <Shimmer classAttributes="h-[302px] rounded-[12px]" />
+      <div className="flex flex-row justify-between mt-[38px]">
+        <div className="flex flex-col justify-between">
+          <Shimmer classAttributes="w-[318px] h-[18px] rounded-[14px]" />
+          <Shimmer classAttributes="w-[212px] h-[18px] rounded-[14px]" />
+        </div>
+        <Shimmer classAttributes="w-[56px] h-[56px] rounded-full" />
+      </div>
+    </div>
+  );
+};
+
+export default DashboardContent;
