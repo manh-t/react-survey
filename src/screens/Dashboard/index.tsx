dc1fefd4e969cb173fbc925c57678029301d1d2e
Tran Manh <manh@nimblehq.co>
2023-06-21 14:59:53 +0700

    [#9] Modify padding top

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -5,9 +5,9 @@ import DashboardHeader from 'components/Dashboard/Header';
 
 const DashBoardScreen = (): JSX.Element => {
   return (
-    <div className="bg-black w-full h-full pt-8 pl-8 pr-8">
+    <div className="bg-black w-full h-full pl-8 pr-8">
       <DashboardHeader dateTime="Monday, JUNE 15" daysAgo="Today" profileUrl="https://i.pravatar.cc/150?img=3">
-        <div className="w-full h-full flex justify-center mt-36">
+        <div className="w-full flex justify-center mt-36">
           <DashboardEmpty />
         </div>
       </DashboardHeader>
