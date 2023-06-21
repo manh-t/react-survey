a4d632657965ee9c30a0e746bf2e1c6ac4ba24b0
Tran Manh <manh@nimblehq.co>
2023-06-21 14:14:29 +0700

    [#5] Add env to build step

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -7,7 +7,7 @@ const HomeScreen = (): JSX.Element => {
     <div className="app">
       <header className="app-header">
         {/* TODO: This is the test line for .env file. Will remove it later. */}
-        <div>{process.env.REACT_APP_DUMMY_ENV}</div>
+        <div>{process.env.REACT_APP_API_BASE_URL}</div>
         <img src={logo} className="app-logo" alt="logo" />
         <a className="app-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" data-test-id="app-link">
           Learn React
