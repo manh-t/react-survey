9743ca2e18ccc673a2dc9ec947fc3e2f42a4534d
Tran Manh <manh@nimblehq.co>
2023-06-27 17:40:19 +0700

    [#11] Update unittest for components

--- src/components/BackgroundImage/index.tsx
+++ src/components/BackgroundImage/index.tsx
@@ -3,16 +3,19 @@ import React from 'react';
 interface BackgroundImageProps {
   children: React.ReactNode;
   backgroundUrl?: string;
+  'data-test-id'?: string;
 }
 
-const BackgroundImage = ({ backgroundUrl, children }: BackgroundImageProps): JSX.Element => {
+const BackgroundImage = ({ backgroundUrl, children, ...attributes }: BackgroundImageProps): JSX.Element => {
   return backgroundUrl ? (
-    <div className={`h-screen w-screen relative`}>
+    <div className={`h-screen w-screen relative`} {...attributes}>
       <img src={backgroundUrl} alt="background" className="absolute w-full h-full z-[-1]" />
       <div className="h-full backdrop-blur-[50px] bg-gradient-to-b from-black/20 to-black">{children}</div>
     </div>
   ) : (
-    <div className="absolute w-full h-full z-[-1] bg-black">{children}</div>
+    <div className="absolute w-full h-full z-[-1] bg-black" {...attributes}>
+      {children}
+    </div>
   );
 };
 
