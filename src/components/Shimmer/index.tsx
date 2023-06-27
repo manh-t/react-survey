3858d239a4360005ff9e1059bafda64f054701cd
Tran Manh <manh@nimblehq.co>
2023-06-27 17:27:13 +0700

    [#10] Update unittest for components

--- src/components/Shimmer/index.tsx
+++ src/components/Shimmer/index.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 interface ShimmerProps {
   classAttributes: string;
+  'data-test-id'?: string;
 }
 
 // Ref: https://delba.dev/blog/animated-loading-skeletons-with-tailwind
-const Shimmer = ({ classAttributes }: ShimmerProps): JSX.Element => {
+const Shimmer = ({ classAttributes, ...attributes }: ShimmerProps): JSX.Element => {
   return (
     <div
       className={`relative before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:from-30% before:via-white/50 before:to-transparent before:to-70% isolate overflow-hidden shadow-xl shadow-black/5 ${classAttributes}`}
+      {...attributes}
     >
       <div className={`bg-white bg-opacity-[.12] ${classAttributes}`}></div>
     </div>
