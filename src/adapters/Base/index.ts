a7f7dfef4d5db2017ca390df3c253d1682c3d5f8
Tran Manh <manh@nimblehq.co>
2023-06-28 11:09:03 +0700

    [#12] Implement the API GET surveys

--- src/adapters/Base/index.ts
+++ src/adapters/Base/index.ts
@@ -3,14 +3,14 @@ import { AxiosRequestConfig } from 'axios';
 import { JSONObject, keysToSnakeCase } from 'helpers/json';
 import requestManager from 'lib/request/v1/requestManager';
 
-export function get(path: string, params?: JSONObject) {
+export const get = (path: string, params?: JSONObject) => {
   const requestOptions: AxiosRequestConfig = {};
   if (params) {
     requestOptions.params = keysToSnakeCase(params);
   }
 
   return requestManager('get', path, requestOptions);
-}
+};
 
 export const post = (path: string, params: JSONObject) => {
   const requestOptions: AxiosRequestConfig = { data: keysToSnakeCase(params) };
--- src/helpers/deserializer.test.ts
+++ src/helpers/deserializer.test.ts
@@ -1,6 +1,6 @@
 import { Resource } from 'types/resource';
 
-import { deserialize } from './deserializer';
+import { deserialize, deserializeList } from './deserializer';
 
 describe('Deserializer helper', () => {
   interface TestType extends Resource {
@@ -24,6 +24,39 @@ describe('Deserializer helper', () => {
       expect(deserializedData.resourceType).toBe('TestType');
       expect(deserializedData.name).toBe('Name');
       expect(deserializedData.age).toBe(25);
+    });
+  });
+
+  describe('deserializeList', () => {
+    const jsonArray = [
+      {
+        id: '1',
+        type: 'TestType',
+        attributes: {
+          name: 'Name',
+          age: 25,
+        },
+      },
+      {
+        id: '2',
+        type: 'TestType',
+        attributes: {
+          name: 'Name',
+          age: 30,
+        },
+      },
+    ];
+
+    it('returns a list of the deserialized items', () => {
+      const deserializedList = deserializeList<TestType>(jsonArray);
+
+      expect(deserializedList).toHaveLength(jsonArray.length);
+
+      deserializedList.forEach((item: TestType, index: number) => {
+        expect(item.resourceType).toBe('TestType');
+        expect(item.name).toEqual(jsonArray[index].attributes.name);
+        expect(item.age).toEqual(jsonArray[index].attributes.age);
+      });
     });
   });
 });
