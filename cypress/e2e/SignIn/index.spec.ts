8e5145ffc95f4139b384b269ceb2d37cdd825496
Tran Manh <manh@nimblehq.co>
2023-07-04 15:08:24 +0700

    [#35] [CHORE] Add the integration test for Sign In screen

--- cypress/e2e/SignIn/index.spec.ts
+++ cypress/e2e/SignIn/index.spec.ts
@@ -1,10 +1,76 @@
-describe('Cypress', () => {
-  it('is working', () => {
-    expect(true).to.equal(true);
+const signInScreenTestIds = {
+  nimbleLogo: 'sign-in__nimble-logo',
+  signInForm: 'sign-in-form',
+  emailLabel: 'sign-in-form__email',
+  emailField: 'sign-in-form__input-email',
+  passwordLabel: 'sign-in-form__password',
+  passwordField: 'sign-in-form__input-password',
+  forgotButton: 'sign-in-form__forgot-button',
+  signInButton: 'sign-in-form__button',
+  errorAlert: 'sign-in__error-alert',
+  loadingDialog: 'sign-in__loading-dialog',
+};
+
+describe('SignIn screen', () => {
+  context('given the valid form submission', () => {
+    beforeEach(() => {
+      cy.interceptWithDelay('GET', '**/api/v1/surveys*', 200, {
+        statusCode: 200,
+        fixture: 'Survey/List/valid.json',
+      });
+      cy.interceptWithDelay('GET', '**/api/v1/me', 200, {
+        statusCode: 200,
+        fixture: 'User/valid.json',
+      });
+    });
+
+    it('shows the loading dialog and navigates to Dashboard screen', () => {
+      cy.interceptWithDelay('POST', '**/api/v1/oauth/token', 200, {
+        statusCode: 200,
+        fixture: 'Authentication/SignIn/valid.json',
+      });
+      cy.visit('/');
+      cy.location().should((location) => {
+        expect(location.pathname).to.equal('/sign-in');
+      });
+
+      cy.findByTestId(signInScreenTestIds.emailField).type('test@email.com');
+      cy.findByTestId(signInScreenTestIds.passwordField).type('12345678');
+      cy.findByTestId(signInScreenTestIds.signInButton).click();
+
+      cy.findByTestId(signInScreenTestIds.loadingDialog).should('be.visible');
+
+      cy.location().should((location) => {
+        expect(location.pathname).to.equal('/');
+      });
+    });
   });
 
-  it('visits the app', () => {
-    cy.visit('/');
-    // TODO Write UI test in integration task
+  context('given the invalid form submission', () => {
+    it('shows the error dialog', () => {
+      cy.interceptWithDelay('POST', '**/api/v1/oauth/token', 200, {
+        statusCode: 400,
+        fixture: 'Authentication/SignIn/invalid.json',
+      });
+      cy.visit('/');
+      cy.findByTestId(signInScreenTestIds.emailField).type('test@email.com');
+      cy.findByTestId(signInScreenTestIds.passwordField).type('12345678');
+      cy.findByTestId(signInScreenTestIds.signInButton).click();
+
+      cy.findByTestId(signInScreenTestIds.loadingDialog).should('be.visible');
+
+      cy.findByTestId(signInScreenTestIds.errorAlert).should('be.visible');
+    });
+  });
+
+  context('given the failed validation form', () => {
+    it('shows the error popup', () => {
+      cy.visit('/');
+      cy.findByTestId(signInScreenTestIds.emailField).type('invalid email');
+      cy.findByTestId(signInScreenTestIds.passwordField).type('12345678');
+      cy.findByTestId(signInScreenTestIds.signInButton).click();
+
+      cy.get('input:invalid').should('have.length', 1);
+    });
   });
 });
